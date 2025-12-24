d302322c4343168b81e3154ae72beea65c8ec8e9
GPetroff1337 <nfsfen16@gmail.com>
2025-12-24 15:51:00 +0200

    Initial project setup

---
+++ src/main.jsx
@@ -0,0 +1,7 @@
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+
+createRoot(document.getElementById('root')).render(
+  <h1 className="text-red-500 text-6xl">Hello world!</h1>
+);
